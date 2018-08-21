36051a0f3e051b5ab18042fe4940b7b0453f3bb1
Giuliano Agria <giuliano.agria@la.logicalis.com>
2018-08-21 13:43:31 -0300

    Aplicando Placar

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ $(function(){
   inicializaContadores();
   inicializaCronometro();
   $("#botao-reiniciar").click(reiniciaJogo);
+  inicializaMarcadores();
 })
 
 function atualizaTamanhoFrase() {
@@ -35,12 +36,28 @@ function inicializaCronometro() {
       tempoRestante--;
       $("#tempo-digitacao").text(tempoRestante);
       if (tempoRestante < 1){
-        campo.attr("disabled", true);
-        $("#botao-reiniciar").attr("disabled", false);
         clearInterval(cronometroID);
-        campo.addClass("campo-desativado");
+        $("#botao-reiniciar").attr("disabled", false);
+        finalizaJogo();
       }
+
     },1000);
+  });
+}
+
+function inicializaMarcadores() {
+  var frase = $(".frase").text();
+  campo.on("input", function() {
+    var digitado = campo.val();
+    var comparavel = frase.substr(0, digitado.length);
+
+  if( frase.startsWith(digitado)) {
+     campo.addClass("borda-verde");
+     campo.removeClass("borda-vermelha");
+    } else {
+     campo.addClass("borda-vermelha");
+     campo.removeClass("borda-verde");
+    }
   });
 }
 
@@ -51,4 +68,13 @@ function reiniciaJogo() {
   $("#contador-caracteres").text("0");
   $("#tempo-digitacao").text(tempoInicial);
   inicializaCronometro();
+  campo.toggleClass("campo-desativado");
+  campo.removeClass("borda-vermelha");
+  campo.removeClass("borda-verde");
+}
+
+function finalizaJogo() {
+  campo.attr("disabled", true);
+  campo.toggleClass("campo-desativado");
+  inserePlacar();
 }
