83d61ef47c2219f100af60c36d1d5b0e396cf4fb
Giuliano Agria <giuliano.agria@la.logicalis.com>
2018-08-21 14:35:24 -0300

    Criando animações no placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
   var corpoTabela = $(".placar").find("tbody");
   var usuario = "Giuliano";
@@ -7,6 +9,17 @@ function inserePlacar() {
   linha.find(".botao-remover").click(removeLinha);
 
   corpoTabela.prepend(linha);
+  $(".placar").slideDown(600);
+  scrollPlacar();
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+
+    $(".body").animate(
+    {
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
@@ -30,6 +43,15 @@ function novaLinha(usuario, palavras) {
 
 function removeLinha() {
     event.preventDefault();
-    $(this).parent().parent().remove();
+    var linha = $(this).parent().parent();
+    linha.fadeOut(1000);
+    setTimeout(function() {
+      linha.remove();
+    },1000);
 
+
+}
+
+function mostraPlacar() {
+  $(".placar").stop().slideToggle(600);
 }
